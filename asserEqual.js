fb40458cda4e7ba6f3400109519500ed1290bf06
Amohamed96 <amohamed.rg@gmail.com>
2021-10-26 08:29:59 -0400

    Added template litterals

--- asserEqual.js
+++ asserEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅ Asserstion Passed: " + actual + " === " + expected);
+    console.log(`✅ Asserstion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("❌ Asserstion Failed: " + actual + " !== " + expected);
+    console.log(`❌ Asserstion Failed: ${actual} !== ${expected}`);
   }
 };
 
